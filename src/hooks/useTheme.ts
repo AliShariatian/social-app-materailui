d2655c12a183b23da5060532cee9ae25de558ef7
Ali Shariatian <shariatian92@gmail.com>
2024-09-05 18:40:44 +0330

    hotfix: change useTheme strategy

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,35 +1,18 @@
 import { create } from "zustand";
 import { PaletteMode } from "@mui/material";
-import { persist } from "zustand/middleware";
-
-const LOCAL_STORAGE_KEY = "theme";
 
 interface IThemeState {
   mode: PaletteMode;
   toggleMode: () => void;
 }
 
-const getInitialMode = (): PaletteMode => {
-  const savedMode = localStorage.getItem(LOCAL_STORAGE_KEY) as PaletteMode;
-  return savedMode === "dark" ? "dark" : "light"; // Default to 'light' if not found
-};
+const useTheme = create<IThemeState>((set) => ({
+  mode: "light", // default mode
 
-const useThemeStore = create(
-  persist<IThemeState>(
-    (set) => ({
-      mode: getInitialMode(),
+  toggleMode: () =>
+    set((state) => ({
+      mode: state.mode === "light" ? "dark" : "light",
+    })),
+}));
 
-      toggleMode: () =>
-        set((state) => {
-          const newMode = state.mode === "light" ? "dark" : "light";
-          return { mode: newMode };
-        }),
-    }),
-    {
-      name: LOCAL_STORAGE_KEY,
-      getStorage: () => localStorage,
-    }
-  )
-);
-
-export default useThemeStore;
+export default useTheme;
